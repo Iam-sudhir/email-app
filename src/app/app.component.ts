0fc5ea4d9f56b412b39085eb924c9a36800fcc98
Swapnasudhir <swapnasudhir@knowledgelens.com>
2021-03-14 12:58:33 +0530

    #Updated Email App

---
+++ src/app/app.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  public showLoader = false;
+  public showMail = false;
+  form = new FormGroup({
+    subject: new FormControl('', Validators.required),
+    message: new FormControl('', Validators.required),
+    email: new FormControl('', [
+      Validators.required,
+      Validators.email
+    ])
+
+  });
+  constructor() { }
+  ngOnInit() {
+  }
+  onSubmit() {
+    this.showLoader = true;
+    setTimeout(() => {
+      this.showMail = true;
+      this.showLoader = false;
+      setTimeout(() => {
+        this.showMail = false;
+        this.resetBtn();
+      }, 10000);
+    }, 15000);
+  }
+  resetBtn() {
+    this.form.reset();
+  }
+}
